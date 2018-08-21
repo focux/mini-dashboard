e50294684196c03aae9bf692fc3e29ee07e2f131
Leonardo Dominguez <focuux@gmail.com>
2018-08-21 17:13:50 -0400

    Add responsiveness

--- src/components/ImageGrid/index.js
+++ src/components/ImageGrid/index.js
@@ -5,7 +5,7 @@ import ImageCard from './ImageCard';
 const ImageGrid = ({ data, onClick, isUploading, clickedImageId }) => (
   <Grid container item xs={12} spacing={8} style={{height: '100%'}}>
     {data.map((v, k) => 
-      <Grid item xs={3} key={k}>
+      <Grid item xs={6} sm={4} md={3} key={k}>
         <ImageCard
           url={v.url}
           name={v.name}
--- src/components/SideMenu/MenuItem/index.js
+++ src/components/SideMenu/MenuItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 import { CustomNavLink, ListItem } from './elements';
 
 const MenuItem = ({ icon, name, path }) => (
@@ -7,7 +7,7 @@ const MenuItem = ({ icon, name, path }) => (
   <CustomNavLink exact to={path}>
     <Grid container justify="space-between">
       <Grid item>{icon}</Grid>
-      <Grid item>{name}</Grid>
+      <Hidden smDown><Grid item>{name}</Grid></Hidden>
     </Grid>
   </CustomNavLink>
   </ListItem>
